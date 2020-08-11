1ee2f2ef78e166b6a5c9f437181cf4767cf5d54a
unknown <m_fattih@hotmail.com>
2020-08-10 20:03:28 -0700

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+
+    mid = (start + end)//2
+    if len(arr) <= 0:
+        return -1
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        end = mid
+        return binary_search(arr, target, start, end)
+    elif arr[mid] < target:
+        start = mid
+        return binary_search(arr, target, start, end)
+    else:
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-
+# def agnostic_binary_search(arr, target):
